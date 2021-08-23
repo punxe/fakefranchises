c8ce5e967f83625d3decd376aad137b3ef89b287
R <dsammsh@gmail.com>
2021-08-22 19:15:38 -0700

    Changed setAllowedOrigins to Azure domain to comply with CORS protocol

--- src/main/java/io/punxe/fakefranchises/web/WebSocketConfig.java
+++ src/main/java/io/punxe/fakefranchises/web/WebSocketConfig.java
@@ -15,7 +15,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
         registry.addEndpoint("/game");
-        registry.addEndpoint("/game").setAllowedOrigins("http://localhost:3000").withSockJS();
+        //registry.addEndpoint("/game").setAllowedOrigins("http://localhost:3000").withSockJS();
+        registry.addEndpoint("/game").setAllowedOrigins("https://fakefranchises.azurewebsites.net").withSockJS();
     }
 
     @Override
